08b1e444c29ffa661135820c5732f64b4b6d3754
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-28 23:41:46 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(input)
+    self.word = input
+  end
+
+  def match(sentence)
+    sorted_word = self.word.split("").sort.join
+
+    sorted_sentences = sentence.collect {|word| word.split("").sort.join}
+
+    sorted_sentences.find_all {|word| word == sorted_word}
+  end
+
+
+end
